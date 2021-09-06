9dcda6b4b153fdb64f437d44e929753ebeaeef9c
Vivek205 <ichbinvivek@gmail.com>
2021-09-06 11:36:54 +0530

    made querystring optional

--- src/functions/tokenSupply/index.ts
+++ src/functions/tokenSupply/index.ts
@@ -11,7 +11,7 @@ export default {
         request: {
           parameters: {
             querystrings: {
-              tokensymbol: true,
+              tokensymbol: false,
             },
           },
         },
